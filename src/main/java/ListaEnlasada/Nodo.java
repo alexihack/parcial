80e472d19019b1797882f3904677f804be4855c4
SISTEMA <SISTEMA@192.168.44.139>
2025-05-26 16:04:37 -0500

    Nodo

---
+++ src/main/java/ListaEnlasada/Nodo.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ListaEnlasada;
+
+/**
+ *
+ * @author SISTEMA
+ */
+
+    public class Nodo {
+    Producto producto;
+    Nodo siguiente;
+
+    public Nodo(Producto producto) {
+        this.producto = producto;
+        this.siguiente = null;
+    }
+}
+
+    
